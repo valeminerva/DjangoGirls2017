8f245ab919ea910968f8a004a12e11f75ee712c9
Valentina Carlini <valentinacarlini@Valentinas-MacBook.local>
2017-12-02 14:48:41 +0100

    Removed middleware classes

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'blog',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
